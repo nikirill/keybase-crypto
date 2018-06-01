bb8ee64b9a7849d9e8633058934828466a52e802
Kirill Nikitin <kirill.nikitin@epfl.ch>
2018-06-01 14:20:49 +0200

    Adding customization for CPU time measurement

--- openpgp/ecdh/ecdh.go
+++ openpgp/ecdh/ecdh.go
@@ -10,6 +10,8 @@ import (
 	"github.com/nikirill/go-crypto/curve25519"
 	"io"
 	"math/big"
+	"log"
+	"syscall"
 )
 
 type PublicKey struct {
@@ -219,7 +221,9 @@ func (e *PublicKey) Encrypt(random io.Reader, kdfParams []byte, plain []byte, ha
 }
 
 func (e *PrivateKey) DecryptShared(X, Y *big.Int) []byte {
+	//m := NewMonitor()
 	Sx, _ := e.Curve.ScalarMult(X, Y, e.X.Bytes())
+	//log.Println("PGP: ", m.Record())
 	return Sx.Bytes()
 }
 
@@ -279,4 +283,45 @@ func Unmarshal(curve elliptic.Curve, data []byte) (x, y *big.Int) {
 	}
 
 	return elliptic.Unmarshal(curve, data)
+}
+
+// Helpers for measurement of CPU cost of operations
+type Monitor struct {
+	CPUtime float64
+}
+
+func NewMonitor() *Monitor {
+	var m Monitor
+	m.CPUtime = getCPUTime()
+	return &m
+}
+
+func (m *Monitor) Reset() {
+	m.CPUtime = getCPUTime()
+}
+
+func (m *Monitor) Record() float64 {
+	return getCPUTime() - m.CPUtime
+}
+
+func (m *Monitor) RecordAndReset() float64 {
+	old := m.CPUtime
+	m.CPUtime = getCPUTime()
+	return m.CPUtime - old
+}
+
+// Returns the sum of the system and the user CPU time used by the current process so far.
+func getCPUTime() float64 {
+	rusage := &syscall.Rusage{}
+	if err := syscall.Getrusage(syscall.RUSAGE_SELF, rusage); err != nil {
+		log.Fatalln("Couldn't get rusage time:", err)
+		return -1
+	}
+	s, u := rusage.Stime, rusage.Utime // system and user time
+	return iiToF(int64(s.Sec), int64(s.Usec)) + iiToF(int64(u.Sec), int64(u.Usec))
+}
+
+// Converts to milliseconds
+func iiToF(sec int64, usec int64) float64 {
+	return float64(sec)*1000.0 + float64(usec)/1000.0
 }
